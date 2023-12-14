a90dc37d3ddbd52285a560cdb87227f238f2e24e
Elif <elifaz2912@gmail.com>
2023-12-14 16:35:20 +0300

    2. Gün lk version

---
+++ src/FirstGitHub.java
@@ -0,0 +1,7 @@
+public class FirstGitHub {
+    public static void main(String[] args) {
+        int a=128;
+        int b=256;
+        System.out.println("İki Sayını Toplamı: " + (a + b));
+    }
+}
